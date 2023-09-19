656b70c453d6abc61493ed10576d54c0fc8aaa1c
axelsameul <emi.duarte643@gmail.com>
2023-09-19 02:29:38 -0300

    cambios en el users.js

--- controlers/users.js
+++ controlers/users.js
@@ -11,14 +11,20 @@ const allUsers = await UserModel.find()/* me devuelve todo los ususarios */
     res.json({msg:"se envian todos los ususarios", allUsers})
 }
 const getOneUsers = async (req,res)=>{
-  const id = req.params.id 
-  const getUser = await UserModel.findOne({_id: id})
- 
+    const getUser = await userModel.findOne({_id: req.params.id})
 
-  res.json({msg:"usuario encontrado", getUser})
+    res.json({msg: "usuario encontrado", getUser})
 }
 
 const createUser = async(req, res)=>{
+
+    const errors = validationResult (req)
+    console.log(errors) 
+
+if (!errors.isEmpty()) {
+    return res.status(422).json({msg: errors.array()})
+}
+
 try {
     const body = req.body
 
@@ -26,6 +32,9 @@ const userExist = await UserModel.findOne({usuario : body.usuario})
 if (userExist) {
    return  res.status(400).json({msg: "el usuario ya existe"})
 }
+
+const salt = await bcrypt.genSaltSync()
+body.contrasenia = await bcrypt.hash(body.contrasenia, salt)
 
 const info = await transporter.sendMail({
     from: '"Fred Foo 👻" <emi.duarte643@gmail.com>', // sender address
@@ -46,7 +55,7 @@ console.log(user)
 
  await user.save()
  await cart.save()
-res.json({msg:"usuario creado correctamente", user})
+res.status(201).json({msg:"usuario creado correctamente", user, status:201})
 
 } catch (error) {
     console.log(error);
@@ -55,15 +64,20 @@ res.json({msg:"usuario creado correctamente", user})
 }
 const uptadeUser = async(req, res)=>{
  
-    const uptadeUser= await UserModel.findByIdAndUpdate({_id: req.params.id}, req.body, {new: true})
-
-    res.status(200).json({msg:"usuario atualizado correctamente",uptadeUser}) 
-}
-const deleteUser = async(req, res)=>{
-   
-    await UserModel.findByIdAndDelete({_id: req.params.id})
-    res.json({msg:"peticion get"},)
-}
+        
+    try {
+        const errors = validationResult (req)
+        console.log(errors)
+        if (!errors.isEmpty()) {
+            res.status(422).json({msg: errors.array()})
+            
+        }
+    const uptadeUser= await userModel.findByIdAndUpdate({_id: req.params.id}, req.body, {new: true})
+    
+        res.status(200).json({msg:"usuario atualizado correctamente",uptadeUser}) 
+    } catch (error) {
+        console.log(error);
+    }}
 
 const logierUser= async(req,res)=>{
     const errors = validationResult (req)
@@ -135,7 +149,14 @@ const logoutUser = async(req,res)=>{
     res.status(200).json({msg :"usuario deslogueado"})
     
     }
-
+    const deleteUser = async(req,res)=>{
+         
+        await userModel.findByIdAndDelete({_id: req.params.id})
+                  
+                   res.json({msg:"se borro corretamente el usuario"})
+                   
+                   
+                   }
 
 
 
