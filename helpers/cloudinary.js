75664d2370a40f69e3f813a8b7f92f61e12f4c03
maurojuarez27851@gmail.com <maurojuarez27851@gmail.com>
2023-10-04 20:51:33 -0300

    se añade codigo para guardar imagenes con cloudinary

---
+++ helpers/cloudinary.js
@@ -0,0 +1,8 @@
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: 'dt1fmrnre',
+    api_key: '653417528193543',
+    api_secret: 'BTILgzRQRVgcZtp6BxF3usRTf-s',
+});
+
+module.exports = cloudinary;
